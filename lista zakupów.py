382dc041d3bd56a37c520cec3828ee22d30971d1
Dmitry Barankov <dmitry.barankov@gmail.com>
2023-02-16 18:56:19 +0100

    I added a loop that prints data from the dictionary

---
+++ lista zakupów.py
@@ -0,0 +1,9 @@
+lista = {
+    "piekarnia": ["Chleb", "Bułka", "Pączek"],
+    "warzywniak": ["Marchew", "Ser", "Rukola"]
+}
+n = 0
+for x, y in lista.items():
+    print(f"Idę do {x.title()}, kupiję tu następujące rzeczy: {y}")
+    n = n + len(y)
+print(f"W sumie kupuję {n} produktów")
